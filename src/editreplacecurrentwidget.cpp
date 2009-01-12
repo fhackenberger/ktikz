c0f009776c1d14be33af78c96858d6c1c9df588e
Glad Deschrijver <glad.deschrijver@gmail.com>
2009-01-12 08:32:40 +0000

    Added src/editreplacecurrentwidget.*
    git-svn-id: file:///tmp/svnroot/ktikz/trunk@48 68211431-5336-0410-8235-f1471971ebd9

---
+++ src/editreplacecurrentwidget.cpp
@@ -0,0 +1,116 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Glad Deschrijver                                *
+ *   Glad.Deschrijver@UGent.be                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QKeyEvent>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+#include "editreplacecurrentwidget.h"
+
+ReplaceCurrentWidget::ReplaceCurrentWidget(QWidget *parent) : QWidget(parent)
+{
+	m_replaceLabel = new QLabel;
+	m_replaceButton = new QPushButton(tr("&Replace"));
+	QPushButton *replaceAllButton = new QPushButton(tr("Replace &All"));
+	QPushButton *dontReplaceButton = new QPushButton(tr("&Don't Replace"));
+	QPushButton *cancelButton = new QPushButton(tr("&Cancel"));
+
+	QVBoxLayout *mainLayout = new QVBoxLayout(this);
+	QWidget *buttonsWidget = new QWidget;
+	QHBoxLayout *buttonsLayout = new QHBoxLayout;
+	buttonsLayout->addWidget(m_replaceButton);
+	buttonsLayout->addWidget(replaceAllButton);
+	buttonsLayout->addWidget(dontReplaceButton);
+	buttonsLayout->addWidget(cancelButton);
+	buttonsLayout->addStretch();
+	buttonsLayout->setMargin(0);
+	buttonsWidget->setLayout(buttonsLayout);
+	mainLayout->addWidget(m_replaceLabel);
+	mainLayout->addWidget(buttonsWidget);
+
+	setFocusProxy(m_replaceButton);
+
+	connect(m_replaceButton, SIGNAL(clicked()), this, SLOT(replace()));
+	connect(replaceAllButton, SIGNAL(clicked()), this, SLOT(replaceAll()));
+	connect(dontReplaceButton, SIGNAL(clicked()), this, SLOT(dontReplace()));
+	connect(cancelButton, SIGNAL(clicked()), this, SLOT(hide()));
+}
+
+ReplaceCurrentWidget::~ReplaceCurrentWidget()
+{
+}
+
+void ReplaceCurrentWidget::setReplacement(const QString &text, const QString &replacement)
+{
+	m_replaceLabel->setText(tr("Replace %1 by %2?").arg(text).arg(replacement));
+}
+
+void ReplaceCurrentWidget::search(const QString &text, const QString &replacement, bool isCaseSensitive, bool findWholeWords, bool forward, bool startAtCursor)
+{
+	m_text = text;
+	m_replacement = replacement;
+	m_isCaseSensitive = isCaseSensitive;
+	m_findWholeWords = findWholeWords;
+	m_forward = forward;
+	m_startAtCursor = startAtCursor;
+	emit search(m_text, m_isCaseSensitive, m_findWholeWords, m_forward, m_startAtCursor);
+}
+
+void ReplaceCurrentWidget::replace()
+{
+	emit replace(m_replacement);
+	emit setSearchFromBegin(false);
+	emit search(m_text, m_isCaseSensitive, m_findWholeWords, m_forward, m_startAtCursor);
+}
+
+void ReplaceCurrentWidget::replaceAll()
+{
+	emit replace(m_replacement);
+	emit replaceAll(m_text, m_replacement, m_isCaseSensitive, m_findWholeWords, m_forward, m_startAtCursor);
+}
+
+void ReplaceCurrentWidget::dontReplace()
+{
+	emit setSearchFromBegin(false);
+	emit search(m_text, m_isCaseSensitive, m_findWholeWords, m_forward, m_startAtCursor);
+}
+
+void ReplaceCurrentWidget::hide()
+{
+	setVisible(false);
+	emit showReplaceWidget();
+}
+
+void ReplaceCurrentWidget::showEvent(QShowEvent *event)
+{
+	m_replaceButton->setFocus();
+	QWidget::showEvent(event);
+}
+
+void ReplaceCurrentWidget::keyPressEvent(QKeyEvent *event)
+{
+	if (event->key() == Qt::Key_Escape)
+		hide();
+	else if (event->key() == Qt::Key_Return)
+		replace();
+	QWidget::keyPressEvent(event);
+}
---
+++ src/editreplacecurrentwidget.h
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Glad Deschrijver                                *
+ *   Glad.Deschrijver@UGent.be                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef REPLACECURRENTWIDGET_H
+#define REPLACECURRENTWIDGET_H
+
+#include <QWidget>
+
+class QLabel;
+class QPushButton;
+
+class ReplaceCurrentWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	ReplaceCurrentWidget(QWidget *parent = 0);
+	~ReplaceCurrentWidget();
+	void setReplacement(const QString &text, const QString &replacement);
+	void search(const QString &text, const QString &replacement, bool isCaseSensitive, bool findWholeWords, bool forward, bool startAtCursor);
+
+signals:
+	void showReplaceWidget();
+	void search(const QString &text, bool isCaseSensitive, bool findWholeWords, bool forward, bool startAtCursor);
+	void replace(const QString &replacement);
+	void replaceAll(const QString &text, const QString &replacement, bool isCaseSensitive, bool findWholeWords, bool forward, bool startAtCursor);
+	void setSearchFromBegin(bool searchFromBegin);
+
+protected:
+	virtual void showEvent(QShowEvent *event);
+	virtual void keyPressEvent(QKeyEvent *event);
+
+protected slots:
+	void hide();
+
+private:
+	QLabel *m_replaceLabel;
+	QPushButton *m_replaceButton;
+	QString m_text, m_replacement;
+	bool m_isCaseSensitive, m_findWholeWords, m_forward, m_startAtCursor;
+
+private slots:
+	void replace();
+	void replaceAll();
+	void dontReplace();
+};
+
+#endif
